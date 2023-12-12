c2448ecac5d5d3df11a4c64d8f61b0b8cfe71611
Kyle Powis <kylepowis@gmail.com>
2023-12-12 18:19:46 +0000

    done task 5

--- src/components/ArticlesCard.jsx
+++ src/components/ArticlesCard.jsx
@@ -1,16 +1,20 @@
-const ArticlesCard = (props) => {
-    const { articles } = props;
+import React from "react";
+import { Link } from "react-router-dom";
 
-    return (
-        <div className="ArticlesCard">
-            {articles.map((article) => (
-            <div key={article.article_id}>
+const ArticlesCard = (props) => {
+  const { articles } = props;
+
+  return (
+    <div className="ArticlesCard">
+      {articles.map((article) => (
+        <div key={article.article_id}>
+          <Link to={`/articles/${article.article_id}`}>
             <h2>{article.title}</h2>
-            <p>{article.body}</p>
-            </div>
-            ))}
+          </Link>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default ArticlesCard;
