a8f1036e4c316c82b5eb8ca50b9e6d61389fed71
Kyle Powis <kylepowis@gmail.com>
2023-12-12 15:04:01 +0000

    added extras to task 4

--- src/components/ArticlesCard.jsx
+++ src/components/ArticlesCard.jsx
@@ -2,13 +2,18 @@ const ArticlesCard = (props) => {
     const { articles } = props;
 
     return (
+        <div>
+        <h1> Articles: </h1>
         <div className="ArticlesCard">
             {articles.map((article) => (
-            <div key={article.article_id}>
+            <div key={article.article_id}> 
             <h2>{article.title}</h2>
             <p>{article.body}</p>
+            <p>{article.votes}</p>
+            
             </div>
             ))}
+        </div>
         </div>
     )
 }
